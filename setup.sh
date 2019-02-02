62023b6e13e634192f5bd848c9ebec7b11b19b8e
mGGk-fr <mggk@protonmail.com>
2019-02-02 18:30:07 +0100

    Info message

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "-----------------------------------------------"
+echo "    Welcome to the PSC PCSX TOOLCHAIN Setup"
+echo "              Version 0.1 by mGGk"
+echo "-----------------------------------------------"
+echo "                    WARNING                    "
+echo "This script is currently in beta and will"
+echo "modify some parts of your system, I advise you"
+echo "to use a VM to use this script......."
+echo "I won't be responsible to any damage done to "
+echo "your system => USE VM / USE SNAPSHOT !"
+echo "-----------------------------------------------"
+echo ""
+echo "Press enter to start setup or make CTLR+C to exit"
+read -n1 -s
