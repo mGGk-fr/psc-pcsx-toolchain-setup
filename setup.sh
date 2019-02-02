d9b40778f60ce984ae7a730ca45bccf958253f7e
mGGk-fr <mggk@protonmail.com>
2019-02-02 19:44:41 +0100

    Reqs

--- setup.sh
+++ setup.sh
@@ -14,3 +14,20 @@ echo "-----------------------------------------------"
 echo ""
 echo "Press enter to start setup or make CTLR+C to exit"
 read -n1 -s
+
+echo ""
+echo "Updating system !"
+sudo apt update
+sudo apt upgrade
+
+echo "Installing requirements"
+sudo apt-get install -y gawk wget git-core diffstat unzip texinfo gcc-multilib \
+     build-essential chrpath socat cpio python python3 python3-pip python3-pexpect \
+     xz-utils debianutils iputils-ping libsdl1.2-dev xterm
+
+echo "Pulling poky"
+git clone git://git.yoctoproject.org/poky
+cd poky
+git fetch --tags
+git checkout tags/yocto-2.5.2 -b psc2.5.2.2
+
